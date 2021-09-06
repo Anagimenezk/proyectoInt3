9cefe3c4318edcce385f1f33a93919658a7c9988
Anagimenezk <gimeneza@udesa.edu.ar>
2021-09-06 11:50:15 -0300

    por las

--- proyecto-int3/src/App.js
+++ proyecto-int3/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Main from './components/Main/Main'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import './App.css';
 
 
 function App() {
