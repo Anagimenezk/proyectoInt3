aeee2e62e2ace738c7d2b653712fd5dfec6b3b63
Anagimenezk <gimeneza@udesa.edu.ar>
2021-09-06 11:44:40 -0300

    seguimos armando archivos, arme main

---
+++ proyecto-int3/src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+import './header.css'
+
+class Main extends Component {
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+    render(){
+        return(
+            <div>
+    
+            </div>
+        )
+    }
+}
+
+
+
+export default Main
+
+
