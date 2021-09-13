823700629359cb8a4e423e713eaf211ea1fb9b9f
Daira Sapir <dairasapir@MacBook-Air-de-Daira.local>
2021-09-12 22:56:02 -0300

    intente cambiar lo de Canciones Populares (que estaba en el footer y lo pase al . header, que igual no era lo que queria hacer) pero me tiraba error asi que solo deje algo comentado y mañana lo vemos bien

--- proyecto-int3/src/App.js
+++ proyecto-int3/src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import Main from './components/Main/Main'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import './App.css';
+//import Card from './components/Card/Card';
 
+
+// por que el h3 lo habian puesto en el footer? lo cambie al header pero igual esta mal..
 
 
 function App() {
   return (
    <div className="App">
      <Header filtrarAlbumes={(textoAFiltrar)=> this.filtrarAlbumes(textoAFiltrar)}/>  
+    
+    <h3 className = "h3"> Canciones populares </h3> 
+     
     <Main/>
     <Footer/>
       <div className = "card-container"></div>
 
-    <h3 className = "h3"> Canciones populares </h3>
+    
 
     </div>
+
   );
+
 }
+
+
+
+
 
 export default App;
