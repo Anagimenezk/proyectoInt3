be95537be6068d70abe4b76d5f0074b7968503b2
Daira Sapir <dairasapir@MacBook-Air-de-Daira.local>
2021-09-15 15:17:19 -0300

    estoy arreglando algunas cosas (errores que tira etc)

--- proyecto-int3/src/components/Header/Header.js
+++ proyecto-int3/src/components/Header/Header.js
@@ -43,17 +43,17 @@ class Header extends Component {
             <React.Fragment>
               
           <header>
-            <h1> <i class="fas fa-record-vinyl"></i> TUNEZAP</h1>
+            <h1> <i className="fas fa-record-vinyl"></i> TUNEZAP</h1>
              <section>
                     <p className="ordenar"> Ordenar ASC/DESC</p>
                    
 
                     <div className="buscador">
-                    <form className = "formbuscador" action="" onSubmit = {(event)=> this.evitarSubmit(event)} >
+                    <form className = "formBuscador" action="" onSubmit = {(event)=> this.evitarSubmit(event)} >
                         <input className = "input-text" type="text" id="" placeholder="Buscar Album"  
                         onChange={(filtro)=> this.controlarCambios(filtro)} value={this.state.filterBy}/>
                         
-                        <button type="submit" className = "botonbuscador"><i className="fas fa-search"></i></button>
+                        <button type="submit" className = "botonBuscador"><i className="fas fa-search"></i></button>
                     </form>
                    
                     </div>
